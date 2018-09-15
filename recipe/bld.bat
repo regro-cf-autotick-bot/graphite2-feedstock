f2fb145f582d0bd38f04b1950569e82f5eff89e2
Peter Williams <peter@newton.cx>
2018-09-15 12:02:08 -0400

    Use MSVC to build on Windows instead of mingw-w64
    This is a C++ project so it makes a difference. The switch was actually very
    easy.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,9 @@
-REM I cannot for the life of me figure out how Cygwin/MSYS2 figures out its
-REM root directory, which it uses to find the /etc/fstab which *sometimes*
-REM affects the choice of the cygdrive prefix. But, regardless of *why*,
-REM I find that we need this to work:
-mkdir %BUILD_PREFIX%\Library\etc
-echo none / cygdrive binary,user 0 0 >%BUILD_PREFIX%\Library\etc\fstab
-echo none /tmp usertemp binary,posix=0 0 0 >>%BUILD_PREFIX%\Library\etc\fstab
-
-set CXXFLAGS=%CXXFLAGS% -std=gnu++17
-set MSYS2_ARG_CONV_EXCL="-DCMAKE_INSTALL_PREFIX="
+@ECHO ON
 
 mkdir build
 cd build
-cmake "-GMSYS Makefiles" ^
+
+cmake -G Ninja ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DBUILD_SHARED_LIBS=ON ^
@@ -19,8 +11,8 @@ cmake "-GMSYS Makefiles" ^
       ..
 if errorlevel 1 exit 1
 
-make
+ninja -v
 if errorlevel 1 exit 1
 
-make install
+ninja install
 if errorlevel 1 exit 1
